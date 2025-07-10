c9efd2f1a789fd5d65a434507670f87095dce7fe
RafaellaMZ <rafaellamacegosa@hotmail.com>
2025-07-10 18:56:14 -0300

    Treino

---
+++ aula25/index.js
@@ -0,0 +1,11 @@
+function fizzBuzz(numero) {
+    if (typeof numero !=='number') return numero;
+    if(numero % 3 === 0 && 5 === 0) return 'Fizz';
+    if (numero % 5 === 0) return 'Buzz';
+    if (numero % 3 === 0) return 'FizzBuzz';
+    return numero
+}
+
+for (let i = 0; i <= 100; i++) {
+    console.log(i, fizzBuzz(i))
+}
